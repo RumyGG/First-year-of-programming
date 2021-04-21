b7740ad39f0988353b40745b363cdd9ad6c88ebf
RumyGG <82515617+RumyGG@users.noreply.github.com>
2021-04-21 11:22:38 +0300

    Add files via upload

---
+++ Using Variables.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    string charecterName = "Tom";
+    int charecterAge = 10;
+    cout << "My name is "<< charecterName << "and Im " << charecterAge << "years old." << endl;
+    return 0;
+}
